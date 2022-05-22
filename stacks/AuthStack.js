0fffc6efe52cbf01f9273a5b764520bb2c504db0
Yao Hui <yaohui12345@hotmail.com>
2022-05-22 15:46:56 +0800

    "added auth stack for linked pages to navigate"

---
+++ stacks/AuthStack.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import LoginPage from '../screens/LoginPage';
+import ForgetPasswordPage from '../screens/ForgetPasswordPage';
+import SignUpPage from '../screens/SignUpPage';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+const AuthStack = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name='login page'
+                    component={LoginPage}
+                />
+                <Stack.Screen
+                    name='forget password page'
+                    component={ForgetPasswordPage}
+                />
+                <Stack.Screen
+                    name='sign up page'
+                    component={SignUpPage}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default AuthStack;
