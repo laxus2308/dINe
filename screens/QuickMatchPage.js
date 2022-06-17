d335b26453d79a7c1d577f11f63b0d0ccd7caae7
Mok Yao Hui <yaohui788@gmail.com>
2022-06-17 23:02:06 +0800

    Quick Match template

---
+++ screens/QuickMatchPage.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+    TouchableOpacity,
+    SafeAreaView,
+    FlatList,
+} from 'react-native';
+import { supabase } from '../supabase';
+
+const QuickMatchPage = () => {
+
+    return (
+        <View>
+            
+        </View>
+    )
+
+}
+
+const styles = StyleSheet.create({
+
+})
+
+
+export default QuickMatchPage;
