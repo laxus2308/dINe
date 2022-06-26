c3719b0b959d8c526beb36df529551ef1b8f08ea
Mok Yao Hui <yaohui788@gmail.com>
2022-06-26 22:54:58 +0800

    added more checks for signup email

--- screens/auth/SignUpPage.js
+++ screens/auth/SignUpPage.js
@@ -17,9 +17,21 @@ const SignUpPage = () => {
   const [confirmPassword, setConfirmPassword] = useState('');
 
   const signUpWithEmailAndPassword = async (e) => {
+    const checkForE = (email) => {
+      return email.charAt(0) == "e";
+    }
+
+    const checkForNus = (email) => {
+      return email.includes('u.nus.edu');
+    }
+
+    const checkLength = (email) => {
+      return email.length == 18;
+    }
+
     if (signUpPassword !== confirmPassword) {
       alert('Passwords must be the same!')
-    } else if (!signUpEmail.includes('u.nus.edu')) {
+    } else if (!checkForE(signUpEmail) && !checkForNus && !checkLength) {
       alert('Nus email required!')
     } else {
       e.preventDefault()
@@ -34,7 +46,8 @@ const SignUpPage = () => {
         const { error: createError } = await supabase
             .from('profiles')
             .insert([
-                { id: user.id, username: 'betatester' }
+                { id: supabase.auth.user.id, 
+                  username: 'betatester' }
             ])
           
         if (error) {
@@ -42,7 +55,7 @@ const SignUpPage = () => {
         } else if (createError) {
           throw createError
         }
-        
+
         alert('Check your email for the login link!')
       } catch (error) {
         alert(error.error_description || error.message)
