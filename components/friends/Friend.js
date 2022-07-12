ae81a55bfbd7ad183d5d504d410c4357ba07b84d
Mok Yao Hui <yaohui788@gmail.com>
2022-07-12 22:26:31 +0800

    "Create new and enter chat from friends system"

--- components/friends/Friend.js
+++ components/friends/Friend.js
@@ -1,27 +1,31 @@
 import { useState, useEffect } from 'react';
 import React from 'react';
-import {Text, Image, View, StyleSheet, TouchableOpacity} from 'react-native';
+import { Text, Image, View, StyleSheet, TouchableOpacity } from 'react-native';
 import { supabase } from '../../supabase';
 import { useNavigation } from '@react-navigation/native';
 
 const Friend = (props) => {
-    const {Friend} = props;
+    const { Friend } = props;
     const [profileImage, setProfileImage] = useState(null);
-    const [username, setUsername] = useState(null);
-    const navigator = useNavigation();
+    const [username, setUsername] = useState();
+    const [chatId, setChatId] = useState();
+    const navigation = useNavigation();
+
+    const user = supabase.auth.user();
 
     useEffect(() => {
         getDetails();
-    })
+        // getChat();
+    },[])
 
-    const getDetails = async() => {
+    const getDetails = async () => {
         try {
-            const {data, error} = await supabase.from('profiles').select().eq('id', Friend.second_id)
-            console.log(Friend.second_id)
+            const { data, error } = await supabase.from('profiles').select().eq('id', Friend.second_id)
             if (data) {
                 setProfileImage(getProfileUri(data[0].avatar_url))
                 setUsername(data[0].username)
             }
+            if (error) throw error
         } catch (error) {
             console.log(error)
         }
@@ -35,37 +39,93 @@ const Friend = (props) => {
             }
             return publicURL;
         } catch (error) {
-            alert('Error downloading image: ', error.message)
+            alert(error.message)
         }
     }
 
+    const getChat = async () => {
+        try {
+            const { data, error } = await supabase
+                .from('friend_relations')
+                .select('chat_id')
+                .match({
+                    first_id: user.id,
+                    second_id: Friend.second_id
+                })
+                .single()
+            if (data.chat_id == null) {
+                await createChat();
+            }else {
+                setChatId(data.chat_id)
+            }
+            if (error) throw error
+        } catch (error) {
+            console.log(error.message)
+        }
+
+    }
+
+    const createChat = async () => {
+        try {
+            const { data, error } = await supabase.rpc('create_chat_room', {
+                profile_id: Friend.second_id
+            })
+            setChatId(data.id)
+
+            if (error) throw error
+        } catch(error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(()=> {
+        if(chatId) {
+            navigation.navigate("Chat", {
+                screen: "ChatRoomPage", 
+                params: {
+                    id: chatId,
+                    name: username,
+                }})
+        }
+    },[chatId] )
+    // const goToChat = () => {
+    //     getChat().then(()=> navigation.navigate("Chat", {
+    //         screen: "ChatRoomPage", 
+    //         params: {
+    //             id: chatId,
+    //             name: username,
+    //         }}))
+    // }
 
     return (
         <View style={styles.requestContainer}>
-            <TouchableOpacity onPress={() => navigator.navigate('ViewProfilePage', {screen:'ViewProfilePage', 
-                params: { 
+            <TouchableOpacity onPress={() => navigation.navigate('ViewProfilePage', {
+                screen: 'ViewProfilePage',
+                params: {
                     profile_id: Friend.second_id,
                     temp: false,
-                    }})}>
+                }
+            })}>
                 <Image
                     source={{ uri: profileImage }}
                     style={styles.avatar}
                 />
             </TouchableOpacity>
             <View style={styles.textContainer}>
-            <TouchableOpacity onPress={() => navigator.navigate('ViewProfilePage', {screen:'ViewProfilePage', 
-                params: { 
-                    profile_id: Friend.second_id,
-                    temp: false,
-                    }})}>
-                <Text style={styles.title}>{username}</Text>
-            </TouchableOpacity>
+                <TouchableOpacity onPress={() => navigation.navigate('ViewProfilePage', {
+                    screen: 'ViewProfilePage',
+                    params: {
+                        profile_id: Friend.second_id,
+                        temp: false,
+                    }
+                })}>
+                    <Text style={styles.title}>{username}</Text>
+                </TouchableOpacity>
             </View>
             <View style={styles.textContainer2}>
-            <TouchableOpacity //onPress={goToChat}
-            >
-                <Image source={require('../../assets/chat.png')} style = {styles.button} />       
-            </TouchableOpacity>
+                <TouchableOpacity onPress={getChat}>
+                    <Image source={require('../../assets/chat.png')} style={styles.button} />
+                </TouchableOpacity>
             </View>
         </View>
     )
